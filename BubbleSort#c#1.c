c11a64c04ef6b42c83996331bab61f290f45daf6
Alzarus <31142128+Alzarus@users.noreply.github.com>
2018-04-12 11:28:49 -0300

    BubbleSort

--- BubbleSort#c#1.c
+++ BubbleSort#c#1.c
@@ -1,6 +1,6 @@
 /*
-Summary: Bubble sort is a simple sorting algorithm that works by repeatedly stepping through the list to be sorted, comparing each pair of adjacent items and swapping them if they are in the wrong order.
-Complexity - O(n^2)
+Descricao: Bubble sort eh um simples algoritmo de ordenacao que funciona atraves da repeticao de passos atraves da lista que deve ser ordenada, comparando cada par de itens adjacentes e invertendo eles caso estejam na ordem incorreta.
+Complexidade - O(n^2)
 */
 
 #include <stdio.h>
@@ -8,44 +8,44 @@ Complexity - O(n^2)
 
 void main()
 {
-	int array[MAXSIZE];
+	int vetor[MAXSIZE];
 	int i, j, num, temp;
 
-	printf("Enter the value of num \n");
+	printf("Digite a quantidade de numeros que deseja inserir: \n");
 	scanf("%d", &num);
 
-	printf("Enter the elements one by one \n");
+	printf("Digite os elementos, um de cada vez: \n");
 	for (i = 0; i < num; i++)
 	{
-		scanf("%d", &array[i]);
+		printf("Elemento %d:", i+1);
+		scanf("%d", &vetor[i]);
 	}
 
-   /*   Bubble sorting begins */
+   /*   Bubble sort iniciado */
 	for (i = 0; i < num; i++)
 	{
 		for (j = 0; j < (num - i - 1); j++)
 		{
-			if (array[j] > array[j + 1])
+			if (vetor[j] > vetor[j + 1])
 			{
-				temp = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = temp;
+				temp = vetor[j];
+				vetor[j] = vetor[j + 1];
+				vetor[j + 1] = temp;
 			}
 		}
 	}
 	
-	printf("Sorted array is...\n");
+	printf("O vetor ordenado ficou...\n");
 	for (i = 0; i < num; i++)
 	{
-		printf("%d\n", array[i]);
+		printf("%d\n", vetor[i]);
 	}
-	getch();
 }
 
 /*
-Input: Enter the value of num
+Entrada: Digite a quantidade de numeros que deseja inserir:
 6
-Enter the elements one by one
+Digite os elementos, um de cada vez:
 23
 45
 67
